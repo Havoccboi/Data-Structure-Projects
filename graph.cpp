f41482cd97a48eb258ee0f86200852c452c466f1
Salman Sahel <salman.exe@gmail.com>
2018-10-12 11:59:16 +0600

    Create graph.cpp

---
+++ graph.cpp
@@ -0,0 +1,48 @@
+  #include <bits/stdc++.h>
+
+using namespace std;
+#define N int(1e5)
+#define pb push_back
+bool visited[N];
+
+vector<vector<int> > ptr(N+1);
+
+int main()
+{
+    int n, m,x,y;
+    cin >> n >> m;
+    for(int i=1;i<n;i++)
+    {
+        cin >> x >> y;
+        ptr[x].pb(y);
+        ptr[y].pb(x);//bidirectional
+    }
+
+
+    cout << "enter root";
+
+    cin >> x;
+    queue<int> q;
+    int level[N]={0};
+
+    q.push(x);
+    while(!q.empty())
+    {
+
+        int s=q.front();
+        cout << s<<endl;
+        q.pop();
+        visited[s]=1;
+        for(auto a:ptr[s])
+        {
+            if(!visited[a])
+            {
+                q.push(a);
+                level[a]=level[s]+1;
+            }
+        }
+
+    }
+
+
+}
