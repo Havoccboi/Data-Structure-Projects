54b74a521c9334f0caf128fa1d04fd27b0c71c68
Salman Sahel <salman.exe@gmail.com>
2018-10-20 11:13:26 +0600

    Update main.cpp

--- Unsortedtypestudent/S/main.cpp
+++ Unsortedtypestudent/S/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include "UnsortedType.h"
 #include "StudentInfo.h"
+
 using namespace std;
 
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    UnsortedType<int> data;
+
+    StudentInfo s(1,"Salman Sahel", 4.00);
+    StudentInfo a(96,"John Doe", 4.20);
+    StudentInfo d(6,"Tony Stark", 10.00);
+
+    data.InsertItem(2);
+    data.InsertItem(1);
+    data.InsertItem(3);
+
+    data.ResetList();
+
+
 }
